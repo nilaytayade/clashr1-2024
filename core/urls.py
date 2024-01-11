82b988e869a86955a474de371e9fbee9342f11e8
nilay <nilaytayade69@gmail.com>
2024-01-11 10:39:42 +0530

    Revert "leaderboard and submit endpoint added"
    This reverts commit 16b7116f62b6a6c5d04f86d2c263eb99d4e6a3b0.

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,5 @@
 from django.urls import path
-from .views import get_mcq,get_leaderboard,submit
+from .views import Get_mcq,test
 urlpatterns=[
-    path('getmcq/',get_mcq),
-    path('getleaderboard/',get_leaderboard),
-    path('submit/',submit),
-    
+    path('getmcq/',Get_mcq),
 ]
