8e8c8ddc07e0366c96ad184c1de17a8dd6cbf3bd
nilay <nilaytayade69@gmail.com>
2024-01-13 22:12:38 +0530

    Simple Jwt added...not implemented yet

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from .views import get_mcq,get_leaderboard,submit
+from .views import get_mcq,get_leaderboard,submit,MyTokenObtainPairView
+
+from rest_framework_simplejwt.views import (
+   
+    TokenRefreshView,
+)
+
 urlpatterns=[
     path('getmcq/',get_mcq),
     path('getleaderboard/',get_leaderboard),
     path('submit/',submit),
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
 ]
--- core/views.py
+++ core/views.py
@@ -3,6 +3,8 @@ from rest_framework.decorators import api_view
 from rest_framework import status
 from .serializers import Mcq_Serializer, Custom_user_Serializer, Submission_Serializer
 from .models import Mcq, Custom_user, Submission
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 
 
@@ -86,3 +88,18 @@ def submit(request):
         return Response(status=status.HTTP_200_OK)
 
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+        # ...
+
+        return token
+    
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class=MyTokenObtainPairSerializer
